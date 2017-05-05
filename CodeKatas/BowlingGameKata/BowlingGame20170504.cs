99754468cb26a8c770f8f6f3eafee7f0809b2c87
Paige Watson <paige.watson@outlook.com>
2017-05-04 22:25:39 -0700

    Added BowlingGame kata

---
+++ CodeKatas/BowlingGameKata/BowlingGame20170504.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKatas.BowlingGameKata
+{
+    [TestClass]
+    public class BowlingGame20170504
+    {
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldScoreGutterGame()
+        {
+            //assign
+            BowlingGame game = new BowlingGame();
+
+            //act
+            int score = game.TotalScore(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            //assert
+            score.Should().Be(0);
+        }
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldScoreAllOnes()
+        {
+            //assign
+            BowlingGame game = new BowlingGame();
+
+            //act
+            int score = game.TotalScore(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+            //assert
+            score.Should().Be(20);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldScoreSpare()
+        {
+            //assign
+            BowlingGame game = new BowlingGame();
+
+            //act
+            int score = game.TotalScore(new int[] { 5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            //assert
+            score.Should().Be(16);
+
+        }
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldScoreStrike()
+        {
+            //assign
+            BowlingGame game = new BowlingGame();
+
+            //act
+            int score = game.TotalScore(new int[] { 10, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            //assert
+            score.Should().Be(24);
+
+        }
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldScorePerfectGame()
+        {
+            //assign
+            BowlingGame game = new BowlingGame();
+
+            //act
+            int score = game.TotalScore(new int[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 });
+            //assert
+            score.Should().Be(300);
+
+        }
+
+
+        private class BowlingGame
+        {
+            public int TotalScore(IReadOnlyList<int> pinsHit)
+            {
+                int score = 0;
+                int numberOfBallsRolled = pinsHit.Count;
+                for (int currentBallNumber = 0; currentBallNumber < numberOfBallsRolled; currentBallNumber++)
+                {
+                    if (score == 300) break;
+                    int currentBallScore = pinsHit[currentBallNumber];
+                    score += currentBallScore;
+                    if (!CanScoreBonus(currentBallNumber, numberOfBallsRolled)) continue;
+                    int nextBallScore = pinsHit[currentBallNumber + 1];
+                    if (!IsStrike(currentBallScore) &&!IsSpare(currentBallScore, nextBallScore)) continue;
+                    int secondBallScore = pinsHit[currentBallNumber + 2];
+                    score += ScoreNextTwoBalls(nextBallScore, secondBallScore);
+                    if(!IsSpare(currentBallScore, nextBallScore))continue;
+                    currentBallNumber++;
+                }
+                return score;
+            }
+            private int ScoreNextTwoBalls(int nextBallScore, int secondBallScore)=>nextBallScore + secondBallScore;
+            private bool IsStrike(int currentBallScore)=>currentBallScore == 10;
+            private bool IsSpare(int currentBallScore, int nextBallScore)=>currentBallScore + nextBallScore == 10;
+            private bool CanScoreBonus(int currentBallNumber, int numberOfBallsRolled)=>(currentBallNumber <= numberOfBallsRolled - 3);
+        }
+    }
+}
---
+++ CodeKatas/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CodeKatas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodeKatas")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("81f76973-9ab1-4511-a782-dba948111993")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
