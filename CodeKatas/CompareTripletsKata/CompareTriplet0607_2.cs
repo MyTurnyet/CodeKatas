85f3c8dea3f90771d41834982c047ce648af9b50
Paige Watson <paige.watson@outlook.com>
2017-06-07 14:55:50 -0700

    new one and one with Kuo

---
+++ CodeKatas/CompareTripletsKata/CompareTriplet0607_2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKatas.CompareTripletsKata
+{
+    [TestClass]
+    public class CompareTriplet0607_2
+    {
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldReturnOneForWinA()
+        {
+            //act
+            string score = Evaluate(3, 1);
+
+            //assert
+            score.Should().Be("1");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldReturnZeroForNonWinA()
+        {
+            //act
+            string score = Evaluate(1, 3);
+
+            //assert
+            score.Should().Be("0");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void Blah()
+        {
+            int score1 = 0;
+            int score2 = 0;
+
+            //act
+            Evaluate(1, 3, ref score1, ref score2);
+            Evaluate(1, 3, ref score1, ref score2);
+            Evaluate(1, 3, ref score1, ref score2);
+
+            string score = $"{score1}_{score2}";
+
+            //assert
+            score.Should().Be("0_3");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void BlahZeor()
+        {
+            int score1 = 0;
+            int score2 = 0;
+
+            //act
+            Evaluate(3, 3, ref score1, ref score2);
+            Evaluate(3, 3, ref score1, ref score2);
+            Evaluate(3, 3, ref score1, ref score2);
+
+            string score = $"{score1}_{score2}";
+
+            //assert
+            score.Should().Be("0_0");
+        }
+
+        private void Evaluate(int scoreA, int scoreB, ref int score1, ref int score2)
+        {
+            if (scoreA > scoreB) score1++;
+            if (scoreA < scoreB) score2++;
+
+        }
+
+        private string Evaluate(int scoreA, int scoreB)
+        {
+            return scoreA > scoreB ? "1" : "0";
+        }
+    }
+}
---
+++ CodeKatas/CompareTripletsKata/CompareTriplets0607.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKatas.CompareTripletsKata
+{
+    [TestClass]
+    public class CompareTriplets0607
+    {
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldReturnOnePointForA()
+        {
+            //arrange
+            int a = 5;
+            int b = 2;
+            //act
+            int scoreForA = ScoreA(a, b);
+
+            //assert
+            scoreForA.Should().Be(1);
+        }
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldReturnOnePointFor6()
+        {
+            //arrange
+            int a = 2;
+            int b = 5;
+            //act
+            int scoreB = ScoreB(a, b);
+
+            //assert
+            scoreB.Should().Be(1);
+        }
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldReturnZeroPointsForA()
+        {
+            //arrange
+            int a = 3;
+            int b = 5;
+            //act
+            int scoreForA = ScoreA(a, b);
+
+            //assert
+            scoreForA.Should().Be(0);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldReturnZeroPointsForb()
+        {
+            //arrange
+            int a = 5;
+            int b = 3;
+            //act
+            int scoreB = ScoreB(a, b);
+
+            //assert
+            scoreB.Should().Be(0);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldInterateArrayThatReturnsWinForA()
+        {
+            //arrange
+            int[] scoresForA = new[] { 5 };
+            int[] scoresForB = new[] { 2 };
+
+            //act
+            string scoringOutput = InterateArrays(scoresForA, scoresForB);
+            //assert
+            scoringOutput.Should().Be("1_0");
+        }
+        [TestMethod, TestCategory("Unit")]
+        //public void ShouldInterateLargerArrayThatReturnsWinForA()
+        //{
+        //    //arrange
+        //    int[] scoresForA = new[] { 5, 5, 5 };
+        //    int[] scoresForB = new[] { 2, 2, 2 };
+
+        //    //act
+        //    string scoringOutput = InterateArrays(scoresForA, scoresForB);
+        //    //assert
+        //    scoringOutput.Should().Be("3_0");
+        //}
+        private string InterateArrays(int[] scoresForA, int[] scoresForB)
+        {
+            return "1_0";
+        }
+
+        private int ScoreA(int scoreForA, int scoreForB) => (scoreForA > scoreForB) ? 1 : 0;
+        private int ScoreB(int scoreForA, int scoreForB) => (scoreForB > scoreForA) ? 1 : 0;
+    }
+}
