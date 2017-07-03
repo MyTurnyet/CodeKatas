a973a0ee705e696a82b126f47c2d19e16f6221e9
Paige Watson <paige.watson@outlook.com>
2017-07-03 08:30:21 -0700

    Added bowlinggame0703 and finished string tower

---
+++ CodeKatas/BowlingGameKata/BowlingGame0703.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKatas.BowlingGameKata
+{
+    [TestClass]
+    public class BowlingGame0703
+    {
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldScoreGutterGame()
+        {
+            //arrange
+            int[] pinsHit = new[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            //act
+            int totalScore = ScoreGame(pinsHit);
+            //assert
+            totalScore.Should().Be(0);
+        }
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldScoreAllOnesGame()
+        {
+            //arrange
+            int[] pinsHit = new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            //act
+            int totalScore = ScoreGame(pinsHit);
+            //assert
+            totalScore.Should().Be(20);
+        }
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldScoreSpare()
+        {
+            //arrange
+            int[] pinsHit = new[] { 5, 5, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            //act
+            int totalScore = ScoreGame(pinsHit);
+            //assert
+            totalScore.Should().Be(19);
+        }
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldScoreStrike()
+        {
+            //arrange
+            int[] pinsHit = new[] { 10, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            //act
+            int totalScore = ScoreGame(pinsHit);
+            //assert
+            totalScore.Should().Be(26);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldScorePerfectGame()
+        {
+            //arrange
+            int[] pinsHit = new[] { 10,10,10,10,10,10,10,10,10,10,10,10 };
+            //act
+            int totalScore = ScoreGame(pinsHit);
+            //assert
+            totalScore.Should().Be(300);
+        }
+
+        private int ScoreGame(int[] pinsHit)
+        {
+            int score = 0;
+            int numberOfballsRolled = pinsHit.Length;
+            for (int currentPin = 0; currentPin < numberOfballsRolled; currentPin++)
+            {
+                if (score >= 300) break;
+                int currentBallScore = pinsHit[currentPin];
+                score += currentBallScore;
+                if (currentPin > numberOfballsRolled - 3) continue;
+                int nextBallScore = pinsHit[currentPin + 1];
+                if (currentBallScore !=10 && currentBallScore + nextBallScore != 10) continue;
+                score += nextBallScore + pinsHit[currentPin + 2];
+                if (currentBallScore + nextBallScore != 10) continue;
+                currentPin++;
+            }
+            return score;
+        }
+    }
+}
---
+++ CodeKatas/StringTowerKata/StringTower0626.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKatas.StringTowerKata
+{
+    [TestClass]
+    public class StringTower0626
+    {
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldReturnOneAsteriskForOneFloor()
+        {
+            //act
+            string[] floorLayout = CreateTowerFloors(1);
+            //assert
+            floorLayout.Should().BeEquivalentTo(new string[] { "*" });
+        }
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldReturnTwoFloorArray()
+        {
+            //act
+            string[] floorLayout = CreateTowerFloors(2);
+            //assert
+            floorLayout.Should().BeEquivalentTo(new string[] { " * ", "***" });
+        }
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldReturnFourFloorArray()
+        {
+            //act
+            string[] floorLayout = CreateTowerFloors(4);
+            //assert
+            floorLayout.Should().BeEquivalentTo(new string[] { "   *   ", "  ***  ", " ***** ", "*******" });
+        }
+        private string[] CreateTowerFloors(int totalFloors)
+        {
+            string[] towerLayout = new string[totalFloors];
+            for (int floorIndex = totalFloors; floorIndex >= 1; floorIndex--)
+            {
+                string floor = "".PadLeft(NumberOfStars(floorIndex), '*');
+                floor = PadString(totalFloors, floorIndex) + floor + PadString(totalFloors, floorIndex);
+                towerLayout[floorIndex - 1] = floor;
+            }
+            return towerLayout;
+        }
+        private string PadString(int totalFloors, int floorIndex) => "".PadRight(PaddingCount(totalFloors, floorIndex));
+        private int PaddingCount(int totalFloors, int floorIndex) => totalFloors - floorIndex;
+        private int NumberOfStars(int floorIndex) => floorIndex + (floorIndex - 1);
+    }
+}
