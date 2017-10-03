327cc3323839cae82f7868c3298350329d0b6bb7
Paige Watson <paige.watson@outlook.com>
2017-10-03 12:56:23 -0700

    Code kata with Mike

---
+++ CodeKatas/BowlingGameKata/BowlingKata20170929.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKatas.BowlingGameKata
+{
+    [TestClass]
+    public class BowlingKata20170929
+    {
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldReturnGutterGame()
+        {
+            //arrange
+            int[] pinsHit = new[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+            //act
+            int score = BowlingScore(pinsHit);
+            //assert
+            score.Should().Be(0);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldtestName()
+        {
+            //arrange
+            
+            //act
+
+            //assert
+        }
+
+        private int BowlingScore(int[] pinsHit)
+        {
+            return 0;
+        }
+    }
+}
