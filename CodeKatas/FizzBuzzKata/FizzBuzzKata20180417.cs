57b606d75cb1fa4e90cf1a8212e987922b36b2d9
Paige Watson <paige.watson@outlook.com>
2018-04-17 15:08:23 -0700

    Last Kata at Premera

---
+++ CodeKatas/FizzBuzzKata/FizzBuzzKata20180417.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKatas.FizzBuzzKata
+{
+    [TestClass]
+    public class FizzBuzzKata20180417
+    {
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldReturnStringForInt() => ConvertIntToString(1).Should().Be("1");
+
+        private string ConvertIntToString(int toBeConverted)
+        {
+            return toBeConverted.ToString();
+        }
+    }
+}
